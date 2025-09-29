764f794c2bb4ede4965421e34a3455020c37fe10
Artem Hroshev <turboplay24@gmail.com>
2025-09-29 20:00:50 +0300

    Adding frontend files

---
+++ frontend/src/App.ts
@@ -0,0 +1,10 @@
+import { BrowserRouter as Router, Routes, Route, Navigate  } from "react-router-dom";
+import LoginPage from "./pages/LoginPage"
+
+function App() {
+    return (
+        
+    )
+}
+
+export default App;
